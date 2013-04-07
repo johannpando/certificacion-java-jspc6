2ffb4c7171453cef1c656c20277667aeb173166c
johannPando@gmail.com <johannPando@gmail.com@b9f34b98-6621-52ca-949e-31c827595772>
2013-04-07 22:06:04 +0000

    jpandode - Ejemplo de como recorrer un mapa.

--- CertificacionJAVA-JSCP/src/temporal/Temporal.java
+++ CertificacionJAVA-JSCP/src/temporal/Temporal.java
@@ -1,7 +1,11 @@
 package temporal;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class Temporal {
 
@@ -15,8 +19,30 @@ public class Temporal {
 		listaString.add("parametro1");
 		listaString.add(null);
 		listaString.add("parametro2");
-		String query = construirQuery(listaString);
-		System.out.println(query);
+		// String query = construirQuery(listaString);
+		// System.out.println(query);
+
+		Map<Integer, String> mapa = new HashMap<Integer, String>();
+
+		mapa.put(1, "Johann");
+		mapa.put(2, "Juan");
+		mapa.put(3, "Carlos");
+
+		Iterator<Entry<Integer, String>> iterator = mapa.entrySet().iterator();
+
+		while (iterator.hasNext()) {
+			Entry<Integer, String> elemento = iterator.next();
+			System.out.println(elemento.getKey() + ": " + elemento.getValue());
+
+		}
+
+		/*
+		 * Collection<String> valores = mapa.values(); Iterator<String> iterador
+		 * = valores.iterator(); boolean hayElementos = iterador.hasNext();
+		 * while (hayElementos) { String elemento = iterador.next();
+		 * System.out.println(elemento); hayElementos = iterador.hasNext(); }
+		 */
+
 	}
 
 	private static String construirQuery(List<String> listaString) {
